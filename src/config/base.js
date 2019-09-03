73f22488108dfe9a5ea6aa8cc897522c486f4132
Pablo Arribas <pablo.arribas@widergy.com>
2019-09-03 15:43:56 -0300

    WIP

--- src/config/base.js
+++ src/config/base.js
@@ -2,7 +2,7 @@
 
 // Settings configured here will be merged into the final config object.
 export default {
-	ACCOUNT_KEY: '',
+	ACCOUNT_KEY: 'NvMxGjtrlTNwTmQ0ad24r44sF8qGase8',
 	// Set to 'docked' or 'normal' for docked or normal mode respectively
 	THEME: 'normal'
 }
